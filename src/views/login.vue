5d1335eb6d4767fae21adb30b9be9e94a22fe9f0
Vitaly Ovsyannikov <degster@ya.ru>
2019-03-09 05:04:23 +0500

    add src/views/login.vue to git

---
+++ src/views/login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login">
+    <img alt="SO MUCH SOBAKA" src="../assets/logo.png">
+    <div>
+      <form class="login" @submit.prevent="login">
+        <h1>Авторизация</h1>
+        <label>Логин</label>
+        <input required v-model="username" type="text" placeholder="Login">
+        <br>
+        <br>
+        <label>Пароль</label>
+        <input
+          required
+          v-model="password"
+          autocomplete="password"
+          type="password"
+          placeholder="Password"
+        >
+        <p>
+          <button type="submit">Login</button>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import cookies from "js-cookie";
+export default {
+  name: "login",
+  methods: {
+    login: function() {
+      const { username, password } = this;
+      if (username === "test" && password === "test") {
+        cookies.set("aut", true);
+        this.$router.push({ name: "map" });
+      } else {
+        alert("Неверные логин или пароль");
+      }
+    }
+  },
+
+  data: function() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  
+  beforeCreate() {
+    this.$router.push({ name: "map" });
+  }
+};
+</script>
